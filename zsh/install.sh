54fcbcba82fe73a7027c03ddbacb41f793ed105c
Andreas Dam Lykkegaard <andreas.dam.lykkegaard@gmail.com>
2025-03-02 18:35:01 +0100

    remove obsolete scripts and update zsh installation process

--- zsh/install.sh
+++ zsh/install.sh
@@ -7,3 +7,8 @@ set -e
 sudo apt update
 sudo apt install -y zsh
 sudo apt-get --only-upgrade -y install zsh
+
+# Check if zsh is the default shell
+if [ "$SHELL" != "/usr/bin/zsh" ]; then
+  sudo chsh -s $(which zsh)
+fi
