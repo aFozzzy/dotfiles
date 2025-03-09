422c4a538a932f4f5ecb1b5191c1a7ac7005977f
Andreas Dam Lykkegaard <andreas.dam.lykkegaard@gmail.com>
2025-03-09 19:47:03 +0100

    enhance PATH and MANPATH exports to prevent duplicates in _path.zsh

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,7 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
-export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+[[ ":$PATH:" != *":./bin:"* ]] && export PATH="./bin:$PATH"
+[[ ":$PATH:" != *":/usr/local/bin:"* ]] && export PATH="/usr/local/bin:$PATH"
+[[ ":$PATH:" != *":/usr/local/sbin:"* ]] && export PATH="/usr/local/sbin:$PATH"
+[[ ":$PATH:" != *":$ZSH/bin:"* ]] && export PATH="$ZSH/bin:$PATH"
+
+[[ ":$MANPATH:" != *":/usr/local/man:"* ]] && export MANPATH="/usr/local/man:$MANPATH"
+[[ ":$MANPATH:" != *":/usr/local/git/man:"* ]] && export MANPATH="/usr/local/git/man:$MANPATH"
